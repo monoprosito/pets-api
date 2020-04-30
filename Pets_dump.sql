0041f98e415854f024fe9cdb6b2c4e012fcfb6bb
MAZTRO <design.mallet@gmail.com>
2020-04-30 21:20:44 +0000

    Add table PET

--- Pets_dump.sql
+++ Pets_dump.sql
@@ -51,7 +51,7 @@ CREATE TABLE owners (
 -- Dumping data for table `owner`
 --
 
--- LOCK TABLES `owner` WRITE;
+LOCK TABLES `owners` WRITE;
 INSERT INTO `owners` (uuid, created_at, updated_at, firstname, lastname)
 VALUES ("521a55f4-7d82-47d9-b54c-a76916479545","2017-03-25 19:42:40","2017-03-25 19:42:40","Jhon","Smith"),
 ("521a55f4-7d82-47d9-b54c-a76916479546","2017-03-25 19:42:40","2017-03-25 19:42:40","Santiago","Arboleda"),
@@ -59,6 +59,7 @@ VALUES ("521a55f4-7d82-47d9-b54c-a76916479545","2017-03-25 19:42:40","2017-03-25
 ("521a55f4-7d82-47d9-b54c-a76916479548","2017-03-25 19:42:40","2017-03-25 19:42:40","Jorge","Salazar"),
 ("521a55f4-7d82-47d9-b54c-a76916479549","2017-03-25 19:42:40","2017-03-25 19:42:40","Julian","Castañeda"),
 ("521a55f4-7d82-47d9-b54c-a76916479550","2017-03-25 19:42:40","2017-03-25 19:42:40","Jonatan","Mazo");
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Pet`
@@ -85,11 +86,14 @@ CREATE TABLE `pets` (
 -- Dumping data for table `Pet`
 --
 
--- LOCK TABLES `pet` WRITE;
+LOCK TABLES `pets` WRITE;
 INSERT INTO `pets` (uuid, created_at, updated_at, name, age, color, owner)
-VALUES ("621a55e4-7d82-47d9-b54c-a76916479551","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",5,"Red","521a55f4-7d82-47d9-b54c-a76916479545")
+VALUES ("621a55e4-7d82-47d9-b54c-a76916479551","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",5,"Red","521a55f4-7d82-47d9-b54c-a76916479545"),
 ("621a55e4-7d82-47d9-b54c-a76916479552","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",3,"Red","521a55f4-7d82-47d9-b54c-a76916479546"),
 ("621a55e4-7d82-47d9-b54c-a76916479553","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",4,"Red","521a55f4-7d82-47d9-b54c-a76916479547"),
 ("621a55e4-7d82-47d9-b54c-a76916479554","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",1,"Red","521a55f4-7d82-47d9-b54c-a76916479548"),
 ("621a55e4-7d82-47d9-b54c-a76916479555","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",6,"Red","521a55f4-7d82-47d9-b54c-a76916479549"),
 ("621a55e4-7d82-47d9-b54c-a76916479556","2017-03-25 19:42:40","2017-03-25 19:42:40","Mayaka",7,"Red","521a55f4-7d82-47d9-b54c-a76916479550");
+UNLOCK TABLES;
+
+-- Dump completed on PLD
